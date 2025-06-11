217e0fbb89dd8abb7b32f061c116c126be262d6a
SALMAN SAFDARI <salmansafdari.1380@gmail.com>
2025-06-11 11:58:06 +0430

    Message board is ready.

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('message/', include('message.urls')),
+     path('', include('message.urls')),
     
 ]
